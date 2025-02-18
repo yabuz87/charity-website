1cf8d8a64c108fc0a68ad0ca27b9f951978e9b95
Yabets solomon <jabezsolomonz@gmail.com>
2025-02-18 21:44:56 +0300

    removed some headers in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import BlogAndNews from "./components/Blogs/BlogAndNews"
 import Signup from './components/SignUp/Signup';
 import Login from "./components/SignUp/Login";
 import Donate from './components/Donate/Donate';
-import Stories from './components/Additional/stories/stories';
+
 
 const App = () => {
   return (
@@ -21,7 +21,6 @@ const App = () => {
       <Route path="/signup" element={<Signup/>}/>
       <Route path="/blog" element={<BlogAndNews/>}/>
       <Route path="/donate" element={<Donate/>}/>
-      <Route path="/stories" element={<Stories/>}/>
       </Routes>
       {/* <Footer/> */}
     </Router>
