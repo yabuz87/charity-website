1275eb47948cd4f59a86290a436acb3eed0f3c3b
Yabets solomon <jabezsolomonz@gmail.com>
2025-03-29 09:05:37 +0300

    Removed submodule clientFrontend completely

--- backend/Router/getRouters.js
+++ backend/Router/getRouters.js
@@ -6,4 +6,4 @@ export const getRouter = express.Router();
 getRouter.get("/home", getHomePage);
 getRouter.get("/galleries", getGalleryPage); // Changed to lowercase 'g' for consistency
 getRouter.get("/blogs", getBlogPage);
-getRouter.get("/projects", getProjectPage);
+getRouter.get("/projects", getProjectPage); 
