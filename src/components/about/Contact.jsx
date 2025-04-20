1171d09a9319cb0b7c401e4a6a81e6a5d36e4b04
Yabets solomon <jabezsolomonz@gmail.com>
2025-04-21 00:29:13 +0300

    added something on contact.jsx

--- src/components/about/Contact.jsx
+++ src/components/about/Contact.jsx
@@ -8,18 +8,21 @@ const Contact = () => {
       <div className="d-flex flex-column align-items-center  justify-content-center rounded-3 p-5" style={{border: "0.2px solid "}}> 
       
       <textarea rows="5" cols="90" className="shadow rounded-3 border p-3"  placeholder="write your message here to us"></textarea>
-      <div className="m-3 w-100" style={{outline:"none"}}> <input type="mail" className=' w-100 p-1 shadow' placeholder="your email here"style={{outline:"none",border:"none"}} /></div>
-      <button className="btn btn-primary px-4">Send</button>
+      <div className="m-3 w-100" style={{outline:"none"}}> <input type="mail" className=' w-100 p-2 shadow rounded-2' placeholder="your email here"style={{outline:"none",border:"none"}} /></div>
+      <button className="btn btn-secondary px-4">Send</button>
       <p><span className="h1">Y</span>ou can also reach out us on different Social Media</p>
       <div className="d-flex  align-items-center justify-content-center">
+      <i className="bi bi-telephone text-dark fs-3 icon m-2" style={{cursor: "pointer"}}></i>
       <i className="bi bi-youtube text-danger fs-3 icon m-2" style={{cursor: "pointer"}}></i>
       <i className="bi bi-telegram text-info fs-3 icon m-2" style={{cursor: "pointer"}}></i>
       <i className="bi bi-facebook text-primary fs-3 icon m-2" style={{cursor: "pointer"}}></i>
       <i className="bi bi-x text-dark fs-3 icon m-2" style={{cursor: "pointer"}}></i>
+      <i className="bi bi-tiktok text-dark fs-3 icon m-2" style={{cursor: "pointer"}}></i>
+      <i className="bi bi-instagram text-danger fs-3 icon m-2" style={{cursor: "pointer"}}></i>
       </div>
       </div>
     </div>
   );
 };
 
-export default Contact;\
+export default Contact;
