beecd3d963aeb419f9df7fb60309c4b5e1562892
Yabets solomon <jabezsolomonz@gmail.com>
2025-06-14 14:43:03 +0300

    edited login.css into Login.css

--- adminFrontend/src/libs/axios.js
+++ adminFrontend/src/libs/axios.js
@@ -2,6 +2,6 @@ import React from 'react'
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:"http://localhost:4000/",
+    baseURL:"https://charity-website-z6po.onrender.com/",
     withCredentials:true,
 });
