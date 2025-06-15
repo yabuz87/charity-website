7827618d9d1ea833838919636efaad99ccfb37a0
Yabets solomon <jabezsolomonz@gmail.com>
2025-06-15 03:56:57 +0300

    modified the generate token and prioritized the cord in index.js

--- backend/index.js
+++ backend/index.js
@@ -15,15 +15,14 @@ import cookieParser from "cookie-parser";
 const app=express();
 const port=process.env.PORT
 
-
-app.use(express.urlencoded({extended:false}));
-app.use(cookieParser());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(express.json());
 app.use(cors({
     origin: ["https://cornelius-charity-org.vercel.app","https://corneliusadmin.vercel.app","https://admin-charity.onrender.com"],
     credentials: true,
 }));
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.json());
 app.get("/",(req,res)=>{
     res.json({"message":"this is a home page"})
 })
--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -1,13 +1,16 @@
 import jwt from "jsonwebtoken";
-export const generateToken=(userId,res)=>{
-    const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d"})
-res.cookie("jwt",token,{
-    maxAge:7*24*60*60*1000,
-    // maxAge:10*60*1000,
-    httpOnly:true,
-    sameSite:"strict",
-    secure:process.env.NODE_ENV !=="development"
 
-})
-return token;
-}
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development", // required for HTTPS in prod
+    sameSite: "None", // ✅ must be None to allow cross-origin cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+
+  return token;
+};
